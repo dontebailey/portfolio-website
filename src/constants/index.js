7a4f923999a86cd77edcb88922d4c6411f0bd9be
dontebailey <baileydonte27@gmail.com>
2024-09-06 14:01:24 -0400

    started hero section of webpage

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+// import project1 from "../assets/projects/project-1.jpg";
+// import project2 from "../assets/projects/project-2.jpg";
+// import project3 from "../assets/projects/project-3.jpg";
+// import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate software developer with a passion for creating useful and aesthetic web applications. I have used my internships and college experiences to become a better developer and I'm honing my skills in front-end technologies like React, HTML, CSS, Tailwind, as well as back-end technologies like Python, Java and SQLite. My goal is to use my experiences to become a better developer so I can contribute to user experiences and develop applications that will positively impact businesses.`;
+export const HERO_CONTENT = `I am a passionate software developer with a passion for creating useful and aesthetic web applications. I have used my internships and college experiences to become a better developer. I am currently honing my skills in front-end technologies like React, HTML, CSS and Tailwind, as well as back-end technologies like Python, Java and SQLite. My goal is to use my experiences to become a better developer so I can contribute to exceptional user experiences and develop applications that will positively impact businesses.`;
 
 export const ABOUT_TEXT = `I am a recent graduate of Bridgewater State University with a strong passion for developing user-friendly applications. Through self-directed study, I am dedicating my spare time to becoming proficient in React, with a focus on building better interfaces. My goal is to secure a full-time position as a software developer, specializing in User Experience, where I can continue to expand my expertise and contribute meaningfully to innovative projects. Outside of coding I love health and fitness, being outdoors and spending time with friends.`;
 
@@ -24,28 +24,28 @@ export const EXPERIENCES = [
   },
 ];
 
-export const PROJECTS = [
-  {
-    title: "Software Developer Jobs GUI",
-    image: project1,
-    description:
-      "A GUI application that used Google Jobs API and an Excel dataset to find software developer jobs in Boston area for user.",
-    technologies: ["Python", "SQLite3", "Dash", "Pandas"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description: "An application for managing completed and uncompleted tasks.",
-    technologies: ["React", "HTML", "CSS"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap", "Tailwind"],
-  },
-];
+// export const PROJECTS = [
+//   {
+//     title: "Software Developer Jobs GUI",
+//     image: project1,
+//     description:
+//       "A GUI application that used Google Jobs API and an Excel dataset to find software developer jobs in Boston area for user.",
+//     technologies: ["Python", "SQLite3", "Dash", "Pandas"],
+//   },
+//   {
+//     title: "Task Management App",
+//     image: project2,
+//     description: "An application for managing completed and uncompleted tasks.",
+//     technologies: ["React", "HTML", "CSS"],
+//   },
+//   {
+//     title: "Portfolio Website",
+//     image: project3,
+//     description:
+//       "A personal portfolio website showcasing projects, skills, and contact information.",
+//     technologies: ["HTML", "CSS", "React", "Bootstrap", "Tailwind"],
+//   },
+// ];
 
 export const CONTACT = {
   address: "Taunton, MA",
